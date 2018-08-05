8e74ff767b47e7f74e7aec9a3635290a842fc77f
jinzhiqiang <jinzhiqiang@maxrocky.com>
2018-08-05 17:01:10 +0800

    修改分页工具

--- src/main/java/com/kingboy/common/utils/page/Page.java
+++ src/main/java/com/kingboy/common/utils/page/Page.java
@@ -9,10 +9,9 @@ import java.util.Objects;
 /**
  * @author kingboy--KingBoyWorld@163.com
  * @date 2017/7/23 下午7:53
- * @param <T> 实体类型 <E>返回体类型
  * @desc  分页容器.
  */
-public class Page<T, E> {
+public class Page {
 
     //当前页
     private Integer current = 1;
@@ -33,14 +32,20 @@ public class Page<T, E> {
         this.size = size;
     }
 
-    public Page(Integer current, Integer size, Long total, List<T> data) {
+    /**
+     * @param <T> 实体类型
+     */
+    public <T> Page(Integer current, Integer size, Long total, List<T> data) {
         this.current = current;
         this.size = size;
         this.total = total;
         this.data = data;
     }
 
-    public void convert(Class<E> clazz) {
+    /**
+     * @param clazz 要转换的类型
+     */
+    public <E> void convert(Class<E> clazz) {
         this.data = MapperUtils.mapperList(data, clazz);
     }
 
